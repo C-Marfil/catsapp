f2b3f775709f19ba39ccd5ab23d3d6bdad8e0823
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-12-17 17:02:16 +0000

    all routes added, awaiting errors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const { Cat } = require('./models');
 const app = express();
 
 app.use(express.json());
 
 app.post('/cats', (req, res) => {
-    res.status(201).json(req.body);
-})
+    Cat.create(req.body).then(cat => res.status(201).json(cat));
+});
+
+app.get('/cats', (req, res) => {
+    Cat.findAll({ where: req.query }).then(cat => res.status(201).json(cat));
+});
+
+app.get('/cats/:id', (req, res) => {
+    Cat.findByPk(req.params.id).then(cat => res.status(201).json(cat));
+});
+
+app.patch('/cats/:id', (req, res) => {
+    Cat.update(req.body, { where: { id: req.params.id } }).then(cat => res.status(200).json(cat));
+});
+
+app.patch('/feed/cat/:id', (req, res) => {
+    const lastFed = { lastFed: new Date() };
+    Cat.update(lastFed, { where: { id: req.params.id } }).then(cat => res.status(201).json(cat));
+});
+
+app.delete('/cats/:id', (req, res) => {
+    const ID_TO_DESTROY = req.params.id;
+
+    Cat.destroy({ where: { id: ID_TO_DESTROY } }).then(cat => res.status(201).json(cat));
+});
+
+
 
 module.exports = app;
--- src/models/index.js
+++ src/models/index.js
@@ -2,11 +2,12 @@ const Sequelize = require('sequelize');
 const CatModel = require('./cats');
 
 const setUpDatabase = () => {
-    const connection = new Sequelize("have_i_fed_the_cat_app", "root", "password", {
+    const connection = new Sequelize("have_i_fed_the_cat_app", "postgres", "password", {
         host: "localhost",
-        port: 3307,
+        port: 5432,
         dialect: "postgres"
-    });
+    })
+
     const Cat = CatModel(connection, Sequelize);
 
     connection.sync({alter: true});
